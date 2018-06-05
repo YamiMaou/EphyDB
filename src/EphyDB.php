f30b0a4f4461afb604970ba6d3d1dde417257a9a
Reginaldo Moreira <reginaldoddm@gmail.com>
2018-06-05 16:45:05 -0300

    Created connection methods

--- src/EphyDB.php
+++ src/EphyDB.php
@@ -1,19 +1,45 @@
 <?php
 
-namespace EphyDB\Adapter;
+namespace EphyDB;
 
 
 class EphyDB
 {
     private $adapter = null;
+
+    /**
+     * @var \PDO
+     */
     private $pdo = null;
 
     public function __construct(\EphyDB\Adapter\Adapter $adapter)
     {
         $this->adapter = $adapter;
         $this->connect();
+    }
 
-        var_dump($this->pdo);
+    /**
+     * @return Select
+     */
+    public function select()
+    {
+        return new Select($this->pdo);
+    }
+
+    public function execute(Select $select) {
+        $stmt = $select->prepare();
+        $stmt->execute();
+        return $stmt;
+    }
+
+    /**
+     * @param $sql
+     * @param int $fetch
+     * @return \PDOStatement
+     */
+    public function query($sql, $fetch = \PDO::FETCH_ASSOC)
+    {
+        return $this->pdo->query($sql, $fetch);
     }
 
     private function connect()
@@ -28,6 +54,13 @@ class EphyDB
             } else {
                 $this->pdo = new \PDO($this->adapter->getDriver());
             }
+
+            if ($this->adapter->getAttributes() && count($this->adapter->getAttributes()) > 0) {
+                foreach ($this->adapter->getAttributes() as $key => $value) {
+                    $this->pdo->setAttribute($key, $value);
+                }
+            }
         }
     }
+
 }
