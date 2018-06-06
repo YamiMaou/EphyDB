4935bc0a25e607f280ab0368f2449ef6fc1d7eea
Reginaldo Moreira <reginaldoddm@gmail.com>
2018-06-06 12:50:27 -0300

    create class delete

---
+++ src/Delete.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace EphyDB;
+
+class Delete implements Preparable
+{
+    /**
+     * @var string
+     */
+    private $query;
+
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * @var string
+     */
+    private $table;
+
+    use Where;
+
+    /**
+     * Delete constructor.
+     * @param \PDO $pdo
+     */
+    public function __construct(\PDO $pdo, $table)
+    {
+        $this->pdo = $pdo;
+
+        if (empty($table)) {
+            throw new \InvalidArgumentException("Table name can not be empty to delete");
+        }
+
+        $this->table = $table;
+    }
+
+    public function __toString()
+    {
+        return $this->query;
+    }
+
+    public function prepare()
+    {
+        $query = sprintf('DELETE FROM %s', $this->table);
+        $query .= (!is_null($this->where)) ? $this->where : '';
+        $this->query = $query;
+
+        $stmt = $this->pdo->prepare($this->query);
+
+        if (count($this->values) > 0) {
+            foreach ($this->values as $key => $value) {
+                $stmt->bindValue($key+1, $value, $this->types[$key]);
+            }
+        }
+
+        return $stmt;
+    }
+}
